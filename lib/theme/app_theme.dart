805a6d0750014174526766b08adc1b7e0d2ca61b
Imanol <taneima05@gmail.com>
2024-02-20 11:58:11 -0600

    Adding GoogleFonts and TextTheme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,33 @@
 //Definicion de los temas de estilo de lapp
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   //constante de color primario
   static const primaryColor = Color.fromARGB(246, 255, 255, 255);
+  //constante d ecolor secundario
+  static const secondaryColor = Colors.black;
   //constante de color de fondo
   static const backColor = Color.fromARGB(246, 151, 229, 237);
   //constante de temas
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-      scaffoldBackgroundColor: backColor,
-      appBarTheme: const AppBarTheme(
+    scaffoldBackgroundColor: backColor,
+    appBarTheme: const AppBarTheme(
+      color: primaryColor,
+    ),
+    textTheme: TextTheme(
+      //titulos muy grandes
+      headlineLarge: GoogleFonts.bebasNeue(
         color: primaryColor,
-      ));
+        fontSize: 26.5,
+        fontWeight: FontWeight.w500,
+        decoration: TextDecoration.underline,
+        decorationColor: const Color.fromARGB(255, 0, 0, 0),
+        decorationStyle: TextDecorationStyle.wavy,
+        decorationThickness: 1.0,
+      ),
+      //estilo para texto muy pequeño
+      bodySmall: GoogleFonts.bebasNeue(color: secondaryColor, fontSize: 16.0),
+    ),
+  );
 }
